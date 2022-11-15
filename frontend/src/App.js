5725ee0d453a384b87577cc0938ea8cfe54e3469
NirmalSankalana <sankalana.nirmal@gmail.com>
2022-11-15 17:55:10 +0530

    Detect duplicated items in the add to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,43 @@
-import data from './data';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import ProductScreen from './screens/ProductScreen';
-import HomeScreen from './screens/HomeScreen';
-import { Badge, Container, Nav, Navbar, NavbarBrand } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import { useContext } from 'react';
-import { Store } from './Store';
+import React from "react";
+import data from "./data";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { Badge, Container, Nav, Navbar, NavbarBrand } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { useContext } from "react";
+import { Store } from "./Store";
 
 function App() {
   const { state } = useContext(Store);
-  const {cart} = state;
+  const { cart } = state;
   return (
-    <BrowserRouter >
-      <div className='d-flex flex-column site-container'>
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Navbar bg='dark' variant='dark'>
+          <Navbar bg="dark" variant="dark">
             <Container>
-              <LinkContainer to='/'>
+              <LinkContainer to="/">
                 <NavbarBrand>amazona</NavbarBrand>
               </LinkContainer>
-              <Nav className='ml-auto'>
-                <Link to='/cart' className='nav-link'>
+              <Nav className="ml-auto">
+                <Link to="/cart" className="nav-link">
                   cart
                   {cart.cartItems.length > 0 && (
-                    <Badge pill bg='danger'>
-                      {cart.cartItems.length}
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
                   )}
                 </Link>
               </Nav>
             </Container>
-
           </Navbar>
         </header>
         <main>
-          <Container className='mt-3'>
+          <Container className="mt-3">
             <Routes>
-              <Route path='/' element={<HomeScreen />} />
-              <Route path='/product/:slug' element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
             </Routes>
           </Container>
         </main>
@@ -50,4 +50,3 @@ function App() {
 }
 
 export default App;
-
